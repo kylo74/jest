197c125a1760145093c8de63d72d6cb308730edc
kylo74 <kyloc74@gmail.com>
2022-03-29 11:04:45 +0000

    Add calculator and button tests

---
+++ scripts/test/calc.test.js
@@ -0,0 +1,25 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const addition = require("../calc");
+
+describe("Calculator", () => {
+    describe("Addition function", () => {
+        test("should return 42 for 20 + 22", () => {
+            expect(addition(20, 22)).toBe(42);
+        });
+        test("should return 41 for 20 + 21", () => {
+            expect(addition(20, 21)).toBe(41);
+        });
+    })
+    describe("Subtraction function", () => {
+        
+    })
+    describe("Multiplication function", () => {
+        
+    })
+    describe("Division function", () => {
+        
+    })
+})
